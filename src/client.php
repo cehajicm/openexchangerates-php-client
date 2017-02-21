f139651c2c3374aec8c23e009e65cb79cd3de981
Mirza Cehajic <cehajic.mirza@nsoft.ba>
2017-02-21 11:03:32 +0100

    initial PHP client infrastructure

---
+++ src/client.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace cehajicm\openexchangerates;
+
+/**
+ * PHP client for the OpenExchangeRates API.
+ *
+ * @version 1.0.0
+ *
+ * @link https://openexchangerates.org/
+ */
+class client
+{
+
+}
